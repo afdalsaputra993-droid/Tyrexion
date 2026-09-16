fcdb54e13ceef3ee18c09b51ff91cfc8c69d5a21
afputra <afdalsaputra993@gmail.com>
2026-09-16 19:52:16 +0700

    perbaikan bug

--- src/app/dashboard/user/page.js
+++ src/app/dashboard/user/page.js
@@ -1,6 +1,6 @@
-import Navbar from "./components/Navbar"
-import Sidebar from "./components/Sidebar"
-import MenuDrawer from "./components/MenuDrawer"
+import Navbar from "../components/Navbar"
+import Sidebar from "../components/Sidebar"
+import MenuDrawer from "../components/MenuDrawer"
 import { SidebarProvider } from "@/context/SidebarContext";
 
 
